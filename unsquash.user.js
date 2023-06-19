531688a2ad6064780a61157bb9a2141cd93ce110
aclist <92275929+aclist@users.noreply.github.com>
2023-06-19 14:10:48 +0900

    Update unsquash.user.js

--- unsquash.user.js
+++ unsquash.user.js
@@ -1,14 +1,16 @@
 // ==UserScript==
-// @name          kbin-unsquash
-// @namespace     https://github.com/aclist/
-// @description   unsquash inline images in comments
-// @author        aclist
-// @version       0.0.5
-// @include       https://kbin.social/*
-// @license       MIT
+// @name                  kbin-unsquash
+// @namespace      https://github.com/aclist/
+// @description      unsquash inline images in comments
+// @author                aclist
+// @version               0.0.6
+// @match                 https://kbin.social/*
+// @license                MIT
+// @downloadURL https://github.com/aclist/kbin-scripts/raw/main/unsquash.user.js
+// @updateURL       https://github.com/aclist/kbin-scripts/raw/main/unsquash.user.js
 // ==/UserScript==
 
-const inlineSelector = 'figure a.thumb img';
+const inlineSelector = 'footer figure a.thumb img';
 const fixedSelector = 'article figure a img';
 
 function updateImg(item, method) {
@@ -17,7 +19,7 @@ function updateImg(item, method) {
     style = 'object-fit: cover !important';
    } else {
        var p = item.parentElement.href;
-        item.src = p;
+       item.src = p;
         style = 'max-width: 50% !important';
    }
     item.style.cssText += style;
@@ -26,10 +28,10 @@ function updateImg(item, method) {
 function checkItems(method) {
     var selector
     if (method == "fixed") {
-          selector = fixedSelector
-      } else {
+          selector = fixedSelector;
+     } else {
           selector = inlineSelector;
-      }
+     }
   const items = document.querySelectorAll(selector);
   items.forEach((item) => {
         updateImg(item,method);
@@ -37,5 +39,7 @@ function checkItems(method) {
 }
 
 /*both fixed size and inline images can exist on profile pages*/
-checkItems("inline")
-checkItems("fixed")
+const imgTypes = ["inline", "fixed"];
+for (let i = 0; i < imgTypes.length; i++) {
+    checkItems(imgTypes[i]);
+}
