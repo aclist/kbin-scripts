a4e99942493ab0941c6d5748218301445a489572
aclist <92275929+aclist@users.noreply.github.com>
2023-06-22 07:47:48 +0900

    chore: add subs test

--- megamod/subs.user.js
+++ megamod/subs.user.js
@@ -6,29 +6,36 @@
 // @author       aclist
 // @match        https://kbin.social/*
 // @license               MIT
-// @downloadURL https://github.com/aclist/kbin-scripts/raw/main/subs.user.js
-// @updateURL      https://github.com/aclist/kbin-scripts/raw/main/subs.user.js
+// @downloadURL https://github.com/aclist/kbin-scripts/raw/main/megamod/subs.user.js
+// @updateURL      https://github.com/aclist/kbin-scripts/raw/main/megamod/subs.user.js
 // ==/UserScript==
 
-var user = document.querySelector('.login');
-var split = user.href.split("/");
-var username = split[4];
-var subLink = 'https://kbin.social/u/' + username + '/subscriptions'
-console.log(username);
-if ( username == null) {
-    return
-} else {
-const myListItem = document.createElement('li');
-const mySubsLink = document.createElement('a');
-mySubsLink.setAttribute('href', subLink);
-mySubsLink.innerText = 'My mags';
-mySubsLink.className = 'subs-nav';
-myListItem.append(mySubsLink);
+function addMags(){
+    var user = document.querySelector('.login');
+    var split = user.href.split("/");
+    var username = split[4];
+    var subLink = 'https://kbin.social/u/' + username + '/subscriptions'
+    console.log(username);
+        if ( username == null) {
+            return
+        } else {
+    const myListItem = document.createElement('li');
+    const mySubsLink = document.createElement('a');
+    mySubsLink.setAttribute('href', subLink);
+    mySubsLink.innerText = 'My mags';
+    mySubsLink.className = 'subs-nav';
+    myListItem.append(mySubsLink);
 
-var nav = document.querySelector('.head-nav__menu');
-var mags = document.querySelector('[href="/magazines"]');
-nav.appendChild(myListItem);
+    var nav = document.querySelector('.head-nav__menu');
+    var mags = document.querySelector('[href="/magazines"]');
+    nav.appendChild(myListItem);
 }
 
-
-
+function addSubs(toggle){
+    console.log(toggle);
+    if (toggle == false) {
+        $('.subs-nav').remove();
+    } else {
+        addMags();
+    }
+}
