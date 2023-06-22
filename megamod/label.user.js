e6bbfac550831b33a8a0f47f0d2ac0b494b027cf
aclist <92275929+aclist@users.noreply.github.com>
2023-06-22 09:12:35 +0900

    fix: set priority

--- megamod/label.user.js
+++ megamod/label.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         kbin-labelop
 // @namespace    https://github.com/aclist
-// @version      0.2
+// @version      0.3
 // @description  label thread author
 // @author       aclist
 // @match        https://kbin.social/*
@@ -10,7 +10,7 @@
 
 function labelOp(toggle){
     if (toggle === false) {
-         document.styleSheets[0].addRule('.own.author > header > .user-inline::after','content: "";background-color:transparent !important;');
+         document.styleSheets[0].addRule('.own.author > header > .user-inline::after','content: "";background-color:transparent !important;padding-left: 0px !important');
     } else {
         document.styleSheets[0].addRule('.own.author > header > .user-inline::after','content: " OP";color:white;background-color:#111;margin-left: 3px;padding-left: 3px;padding-right: 3px');
     }
